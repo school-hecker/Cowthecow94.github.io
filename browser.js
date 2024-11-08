56f4ec8a5a45e8a54c1a775e855ee94d99f09b7c
A_Fish (Hacker) <164597423+rasafhw@users.noreply.github.com>
2024-11-08 17:22:29 -0600

    Create browser.js

---
+++ browser.js
@@ -0,0 +1,6 @@
+// ANhamdsT/5FFQOpBSSfsSzXWPKaFcuo6fRFv47H6WwIDueS5K5yIJ65R3TQ3ArO4sFsz/UN9fqwaJvX728bxGvrbC1Qt0L2n/xQVovP9oG0ORe55wCN7YV/aB4jd5OlIJqiBIoUIYh0wX8K3rDds2k25NijYQCl+IdAXqMJUeBMPktA6SPMV2F/4iO1OJPFjmDG2s+v+4mypZjKAXifR8CYDZFTsTQ95QNtsWvszTtUpdNJ7Pkwh3GALldAtgEXwWaLbC/0VBPxcA45VF4jySpiZeic3H1jgje94PnvkSNDqbwA4tvARyfrH4Ht4iA/lbKOrAUfAi/aDAcAZHZu4yw==
+/**
+ ** Copyright (C) 2024 Opera Norway AS. All rights reserved.
+ **
+ ** This file is part of the Opera web browser.
+ **
